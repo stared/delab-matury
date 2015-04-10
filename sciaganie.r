f2d54b858306827d4a37bddfa6aebecbbbca15e6
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2015-04-10 20:16:53 +0200

    funkcja do sciagania wynikow wszystkich matur + histogramy w osobnym skrypcie

--- sciaganie.r
+++ sciaganie.r
@@ -1,43 +1,43 @@
+# Pobiera danych o wynikach matur i zapisuje do utworzonego katalogu "wyniki" w postaci plików .csv.
+# Również sciąga i zapisuje testy
+# Jeśli pobrane wyniki już tam są, pobiera ponownie.
+# Przed odpaleniem skryptu lepiej się upewnić, czy working directory jest właściwe.
 
-# Pobieranie danych o wynikach matur, na razie tylko zdefiniowana funkcja
 library(ZPD)
 library(ggplot2)
 
+dir.create("wyniki", showWarnings = FALSE)
+setwd("wyniki")
+
+src <- polacz()
+testy <- pobierz_testy(src)
+testy <- testy %>% collect()
+write.csv(testy, "testy.csv")
+
+#rodzaje matur i lata:
+rodzaj <-c("matura", "matura poprawkowa")
+czesc <- unique(testy$czesc_egzaminu[testy$rodzaj_egzaminu %in% rodzaj])
+czesc <- czesc[!is.na(czesc)]
+
+lata <- unique(testy$rok[testy$rodzaj_egzaminu %in% rodzaj])
+lata <- lata[!is.na(lata)]
+
+powtCzesc <- rep(czesc, length(lata))
+powtLata <- sort(rep(lata, length(czesc)))
+
+# funkcja pobierająca wyniki konkretnej matury
 download_wyniki <- function(typ_matury, rok){
+  print(paste("pobieranie ", typ_matury, " ", rok))
   src = polacz()
   wyniki <- pobierz_wyniki_egzaminu(src, "matura", typ_matury, rok, T)
   wyniki <- wyniki %>% collect() # pobranie wyników z bazy danych na komputer
   file_name <- paste(paste(gsub(" ", "_", typ_matury), rok, sep="_"), "csv", sep=".")
   write.csv(wyniki, file_name)
+  rm(wyniki, file_name)
 }
 
-# Proby zabawy z wynikami - matura z biologii rozszerzona 
-src = polacz()
-bio_r_2014 <-pobierz_wyniki_egzaminu(src, "matura", "biologia rozszerzona", 2014, T)
+mapply(download_wyniki, czesci, lata)
 
-bio_r_2014 <- bio_r_2014 %>% collect() # pobranie wyników z bazy danych na komputer
-pytania <- bio_r_2014[,grep("^k_[0-9]*", names(bio_r_2014))]
-sum_wynik <- rowSums(pytania)
-procent_wynik <- 100 * sum_wynik/max(sum_wynik)
-hist(procent_wynik)
+# uczniowieTesty pobierają się za długo
 
-ggplot(data.frame(sum_wynik), aes(x=sum_wynik)) + geom_histogram(colour="white", binwidth=1)
-
-
-# Proby zabawy z wynikami - matura z polskiego podstawowa 
-src = polacz()
-pl_2014 <-pobierz_wyniki_egzaminu(src, "matura", "j. polski podstawowa", 2014, T)
-pl_2014 <- pl_2014 %>% collect() # pobranie wyników z bazy danych na komputer
-pytania <- pl_2014[,grep("^k_[0-9]*", names(pl_2014))]
-sum_wynik <- rowSums(pytania, na.rm=T)
-procent_wynik <- 100 * sum_wynik/max(sum_wynik)
-ggplot(data.frame(sum_wynik), aes(x=sum_wynik)) +
-  geom_histogram(colour="white", binwidth=1) +
-  xlab("suma punktów") +
-  ylab("liczba uczniów") +
-  ggtitle("Wyniki matury podstawowej z j. polskiego w 2014 roku")
-
-# inne
-src = polacz()
-uczniowie = pobierz_uczniow(src)
-
+setwd("../")
