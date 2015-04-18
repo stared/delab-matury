ce9f2335c44273d304dc9790fcd0861204c9b7c1
Piotr Migdal <pmigdal@gmail.com>
2015-04-18 15:37:49 +0200

    wyniki laureatow

--- eksploracje/mat_vs_pol.r
+++ eksploracje/mat_vs_pol.r
@@ -14,10 +14,15 @@ pol_podst <- pol_podst %>% select(-starts_with("k_"))
 ggplot(mat_podst, aes(x=suma)) + geom_histogram(binwidth = 1)
 ggplot(pol_podst, aes(x=suma)) + geom_histogram(binwidth = 1)
 
-# lub bez joinow
-pm = inner_join(mat_podst, pol_podst, by='id_obserwacji')
+# lub bez joinow by uniknac tez zmiany nazw?
+pm <- inner_join(mat_podst, pol_podst, by='id_obserwacji') %>%
+  rename(wynik_mat=suma.x, wynik_pol=suma.y, laureat_mat=laureat.x, laureat_pol=laureat.y)
 
-qplot(suma.x, suma.y, data=pm, geom='bin2d',
-      bins=c(max(pm$suma.x) + 1, max(pm$suma.y) + 1), drop=FALSE)
+qplot(wynik_mat, wynik_pol, data=pm, geom='bin2d',
+      binwidth=c(1, 1), drop=FALSE)
 
+ggplot(pm, aes(x=wynik_mat, y=..density.., fill=laureat_pol)) +
+  geom_histogram(binwidth=1, alpha=0.5, position="identity")
 
+ggplot(pm, aes(x=wynik_pol, y=..density.., fill=laureat_mat)) +
+  geom_histogram(binwidth=1, alpha=0.5, position="identity")
