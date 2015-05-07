5d1fe66543d9294097c121504265486b421978b4
Marta Czarnocka-Cieciura <martaczc@interia.pl>
2015-05-07 10:43:11 +0200

    progres

--- eksploracje/hist_shiny/server.R
+++ eksploracje/hist_shiny/server.R
@@ -3,58 +3,59 @@ library(ggplot2)
 library(dplyr)
 
 # TODO: dane wczytywane w progress bar nie są widoczne! Jak to polaczyc?
-# sa bledy
-dane <- read.csv("../../dane/przetworzone/sumy_laureaty.csv")
-
-# histogram bez podziału na grupy
-ggHistWszyscy<-function(nazwa) {
-  sum_wynik <- dane[,nazwa]
-  procent_wynik <- data.frame(100 * sum_wynik/max(sum_wynik, na.rm=T))
-  names(procent_wynik) <- c("procent_wynik")
-  krok <- 100 * 1/max(sum_wynik,  na.rm=T)
-  p <- ggplot(procent_wynik, aes(x=procent_wynik, y = ..density.. * 100)) +
-    geom_histogram(colour="white", binwidth=krok) +
-    xlab("% punktów") +
-    ylab("% uczniów") +
-    ggtitle(gsub("^j_", "j. ", nazwa) %>% gsub("_", " ", .)) 
-  return(p)
-}
-
-# histogram z podziałem na grupy
-ggHistPodzial<-function(nazwa, filtr, data=dane, tytul_legendy=filtr, zamienNA=NA, kolory=c("red", "blue")){
-  dane_zmod <- data
-  if (is.na(zamienNA)) {
-    # usuwa wiersze z NA w kolumnie filtr
-    dane_zmod <- dane_zmod[!is.na(dane_zmod[,filtr]),]
-  }
-  else{
-    # zamienia NA w kolumnie filtr na 'zmianNA'
-    dane_zmod[is.na(dane_zmod[,filtr]), filtr] <- zmienNA 
-  }
-  sum_wynik <- dane_zmod[nazwa]  
-  procent_wynik <- 100 * sum_wynik/max(sum_wynik, na.rm=T)
-  filtr_dane <- dane_zmod[,filtr]
-  dane_wybrane <- data.frame(procent_wynik, filtr_dane)
-  colnames(dane_wybrane) <- c("procent_wynik", "filtr_dane")
-  krok <- 100 * 1/max(sum_wynik,  na.rm=T)
-  p <- ggplot(dane_wybrane, aes(x=procent_wynik, fill=filtr_dane, y=..density.. * 100)) +
-    scale_fill_manual(values=kolory, name=filtr_dane) +
-    geom_histogram(binwidth=krok, binwidth=.5, alpha=.3, position="identity") +
-    xlab("% punktów") +
-    ylab("% uczniów") +
-    guides(fill=guide_legend(title=tytul_legendy)) +
-    ggtitle(gsub("^j_", "j. ", nazwa) %>% gsub("_", " ", .))   
-  return(p)
-}
+# dane <- read.csv("../../dane/przetworzone/sumy_laureaty.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-    
+  
+#   dane <- read.csv("../../dane/przetworzone/sumy_laureaty.csv")
+  
   # pokazuje progress przy obliczeniach
   withProgress(message = 'Wczytuję wyniki,',
                detail = 'Może chwilę potrwać...', value = 0, {
-                 #dane <- read.csv("../../dane/przetworzone/sumy_laureaty.csv")
+                 dane <- read.csv("../../dane/przetworzone/sumy_laureaty.csv")
                })  
+  
+  # histogram bez podziału na grupy
+  ggHistWszyscy<-function(nazwa) {
+    sum_wynik <- dane[,nazwa]
+    procent_wynik <- data.frame(100 * sum_wynik/max(sum_wynik, na.rm=T))
+    names(procent_wynik) <- c("procent_wynik")
+    krok <- 100 * 1/max(sum_wynik,  na.rm=T)
+    p <- ggplot(procent_wynik, aes(x=procent_wynik, y = ..density.. * 100)) +
+      geom_histogram(colour="white", binwidth=krok) +
+      xlab("% punktów") +
+      ylab("% uczniów") +
+      ggtitle(gsub("^j_", "j. ", nazwa) %>% gsub("_", " ", .)) 
+    return(p)
+  }
+  
+  # histogram z podziałem na grupy
+  ggHistPodzial<-function(nazwa, filtr, data=dane, tytul_legendy=filtr, zamienNA=NA, kolory=c("red", "blue")){
+    dane_zmod <- data
+    if (is.na(zamienNA)) {
+      # usuwa wiersze z NA w kolumnie filtr
+      dane_zmod <- dane_zmod[!is.na(dane_zmod[,filtr]),]
+    }
+    else{
+      # zamienia NA w kolumnie filtr na 'zmianNA'
+      dane_zmod[is.na(dane_zmod[,filtr]), filtr] <- zmienNA 
+    }
+    sum_wynik <- dane_zmod[nazwa]  
+    procent_wynik <- 100 * sum_wynik/max(sum_wynik, na.rm=T)
+    filtr_dane <- dane_zmod[,filtr]
+    dane_wybrane <- data.frame(procent_wynik, filtr_dane)
+    colnames(dane_wybrane) <- c("procent_wynik", "filtr_dane")
+    krok <- 100 * 1/max(sum_wynik,  na.rm=T)
+    p <- ggplot(dane_wybrane, aes(x=procent_wynik, fill=filtr_dane, y=..density.. * 100)) +
+      scale_fill_manual(values=kolory, name=filtr_dane) +
+      geom_histogram(binwidth=krok, binwidth=.5, alpha=.3, position="identity") +
+      xlab("% punktów") +
+      ylab("% uczniów") +
+      guides(fill=guide_legend(title=tytul_legendy)) +
+      ggtitle(gsub("^j_", "j. ", nazwa) %>% gsub("_", " ", .))   
+    return(p)
+  }
   
   output$ggHistMatury <- renderPlot({
     #cat("start \n", file = stderr())
