c7387a3b373cb9101018d3ff5b4caabbe8c44c2e
Piotr Migdal <pmigdal@gmail.com>
2015-05-13 22:56:07 +0200

    wyrzucanie poprawiajacych

--- eksploracje/porownanie_przedmiotow.r
+++ eksploracje/porownanie_przedmiotow.r
@@ -22,35 +22,20 @@ matury$nazwa %>%
 
 # indeksy, informacje ogolne
 przedmioty <- read.csv("../dane/wyniki/j._polski_podstawowa_2014.csv")
-# trzeba wyrzucac poprawkowe
-przedmioty[1000:1010,"pop_podejscie"]
+
+# wyrzucamy poprawkowe
+przedmioty <- przedmioty %>% filter(is.na(pop_podejscie))
+
 rownames(przedmioty) <- przedmioty$id_obserwacji
 przedmioty <- select(przedmioty, id_szkoly, plec, rocznik, dysleksja)
 
-# # no i cos nie wrzuca...
-# apply(matury, 1, function (row) {
-#   
-#   print(row["sciezka"])
-#   
-#   matura <- read.csv(row["sciezka"])
-#   rownames(matura) <- matura$id_obserwacji
-# 
-#   przedmioty[rownames(matura), paste0(row["przedmiot"], "_laureat")] <- matura$laureat
-#   przedmioty[rownames(matura), row["nazwa"]] <- matura %>%
-#     select(starts_with("k_")) %>% rowSums(na.rm=T)
-#   
-#   TRUE
-#   
-# })
-
-# to odziala!
 for (i in 1:nrow(matury)) {
 
   row <- matury[i,]
   print(row$sciezka)
   
-  matura <- read.csv(row$sciezka)
-  # trzeba wyrzucac poprawkowe
+  matura <- read.csv(row$sciezka) %>% filter(is.na(pop_podejscie))
+
   rownames(matura) <- matura$id_obserwacji
   
   przedmioty[rownames(matura), paste0(row$przedmiot, "_laureat")] <- matura$laureat
@@ -61,5 +46,5 @@ for (i in 1:nrow(matury)) {
 dir.create("../dane/przetworzone/", recursive=TRUE)
 write.csv(przedmioty, "../dane/przetworzone/sumy_laureaty.csv")
 
-
+# dane bez poprawiajacych
 
