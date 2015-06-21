edb813d981417250b4a41417fa61e1620dba9ff2
Piotr Migdal <pmigdal@gmail.com>
2015-06-21 13:58:05 +0200

    etykiety, kolor i czesc polaczen

--- d3viz/js/coincidence_graph.js
+++ d3viz/js/coincidence_graph.js
@@ -20,24 +20,33 @@ function CoincidenceGraph(selector) {
 
   this.draw = function (graph) {
 
+    graph.links = graph.links.sort(function (a, b) {
+      return b.liczba - a.liczba;
+    });
+
+    graph.links = graph.links.filter(function (e) {
+      return e.oe > 1.25; // || e.oe < 0.5;
+    });
+
     var sizeScale = d3.scale.sqrt()
       .domain([0, d3.max(graph.nodes, function (d) { return d.liczba; })])
       .range([0, 75]);
 
     var maxOe = d3.max(graph.links, function (e) { return e.oe; });
 
-    var colors = d3.scale.category10();
+    var colors = d3.scale.category10()
+      .domain(d3.range(0, 10));
 
     var force = d3.layout.force()
-        .charge(function (d) { return -50 * sizeScale(d.liczba); })
-        .linkDistance(60)
-        .gravity(0.3)
+        .charge(function (d) { return -75 * sizeScale(d.liczba); })
+        .linkDistance(0)
+        .gravity(0.5)
         .size([width, height])
-        .nodes(graph.nodes)
-        .links(graph.links)
         .linkStrength(function (e) {
           return e.oe > 1 ? (e.oe - 1) / (maxOe - 1) : 0;
-        });
+        })
+        .nodes(graph.nodes)
+        .links(graph.links);
 
     var link = svg.selectAll(".link")
       .data(graph.links)  // sort it
@@ -57,13 +66,22 @@ function CoincidenceGraph(selector) {
       .enter().append("circle")
         .attr("class", "node")
         .attr("r", function (d) { return sizeScale(d.liczba); })
-        .style("fill", colors(1))
+        .style("fill", function (d) {
+          return (d.nazwa.indexOf("rozszerzona") !== -1) ? colors(4) : colors(0);
+        })
         .on("mouseover", function (d) {
           tooltip.show([d.nazwa, d.liczba].join("<br>"));
         })
         .on("mouseout", function () {
           tooltip.out();
         });
+
+    var label = svg.selectAll(".label")
+      .data(graph.nodes)
+      .enter().append("text")
+        .attr("class", "label")
+        .style("font-size", function (d) { return 0.7 * sizeScale(d.liczba); })
+        .text(function (d) { return d.label; });
 
     var drag = force.drag();
     node.call(drag);
@@ -73,6 +91,9 @@ function CoincidenceGraph(selector) {
     force.on("tick", function() {
         node.attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
+
+        label.attr("x", function(d) { return d.x; })
+             .attr("y", function(d) { return d.y; });
 
         link.attr("x1", function(e) { return e.source.x; })
             .attr("y1", function(e) { return e.source.y; })
--- d3viz/js/main.js
+++ d3viz/js/main.js
@@ -4,5 +4,15 @@ var cGraph = new CoincidenceGraph("#d3graph");
 // cGraph.loadCSV("data/wspolwyst_matury2014.csv");
 
 d3.json("data/wspolwyst_matury2014.json", function (error, data) {
+
+  // dodawanie etykiet
+  data.nodes.forEach(function (node) {
+    if (node.nazwa.indexOf("j_") !== -1) {
+      node.label = node.nazwa.slice(2, 5).toUpperCase();
+    } else {
+      node.label = node.nazwa.slice(0, 3).toUpperCase();
+    }
+  });
+
   cGraph.draw(data);
 });
